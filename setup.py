55a3bc87b2a2eab60ffc9e2ec34f1361e6bb66ca
Deepanshu Yadav <d2810201@gmail.com>
2026-08-13 23:20:41 +0530

    successfully implemnted logger and exception handler in the proejct

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="deepanshu yadav",
+    author_email="d2810201@gmail.com",
+    packages=find_packages()
+)
